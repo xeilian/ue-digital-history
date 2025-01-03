e51ab19fad0366ad93c7f5f13c1453e15119c388
Maximilian Hege <141145150+xeilian@users.noreply.github.com>
2025-01-03 14:59:29 +0100

    Create ue_digital_history_aufgabe.py

---
+++ ue_digital_history_aufgabe.py
@@ -0,0 +1,27 @@
+# Kleine Python-Datei für Ü Digital History (03.01.2025)
+
+# Datentypen
+var = 1			# integer
+var2 = "1"		# string
+var3 = True		# boolean
+var4 = 3.2		# float
+var5 = [1,2,3]		# list
+var6 = {"key": "value"}	# dict
+
+# if
+alter = input("Wie alt bist du? ")
+if alter >= 18:
+    print("Du kannst eintreten")
+else:
+    print("Haha, nice try")
+
+# while
+var7 = 1
+while var7 <= 10:
+    print(var5)
+    var5 += 1 # Printet die Zahlen 1 bis 10 in die Konsole
+
+# for 
+var8 = ["Hallo", "Welt"]
+for i in var8:
+    print(var8) # Printet erst 'Hallo', dann 'Welt' in die Konsole
